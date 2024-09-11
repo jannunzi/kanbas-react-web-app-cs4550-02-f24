68e8502a7b872eb43405a0f32b6ab742a3cccebf
Jose Annunziato <jannunzi@gmail.com>
2024-09-10 20:34:55 -0400

    heading exercise

--- src/Labs/Lab1.tsx
+++ src/Labs/Lab1.tsx
@@ -2,7 +2,9 @@ export default function Lab1() {
   return (
     <div>
       <h2>Lab 1</h2>
-      <h3>Exercise 1.1</h3>
+      <h3>Headings</h3>
+      <h1>H1 is the largest heading</h1>
+      <h6>And H6 is the smallest</h6>
       <h3>Exercise 1.2</h3>
     </div>
   );
