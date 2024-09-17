b4f557b681eb19ef9068cc3dbaf87b25ab65920a
Jose Annunziato <jannunzi@gmail.com>
2024-09-17 17:45:20 -0400

    assignment 1 lab exercises

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,11 +1,9 @@
 import Lab1 from "./Lab1";
-import Lab2 from "./Lab2";
-
 export default function Labs() {
   return (
-    <div>
+    <div id="wd-labs">
+      <h1>Labs</h1>
       <Lab1 />
-      <Lab2 />
     </div>
   );
 }
